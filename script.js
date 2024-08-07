a76085288c42621e409398598d575127e2d5f662
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2024-08-07 10:33:52 +0200

    (feat) more design

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.querySelector('form').addEventListener('submit', (event => {
-    event.preventDefault()
-}))
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault()
+  const formElements = document.querySelectorAll('form *')
+  formElements.forEach((el) => (el.style.display = 'none'))
+
+  document
+    .querySelectorAll('#toast, #toast *')
+    .forEach((el) => (el.style.display = 'block'))
+})
+
+document.querySelector('form button').addEventListener('click', () => {
+  document
+    .querySelectorAll('form input, form textarea, form label:has(input[type="radio"])')
+    .forEach((el) => el.classList.add('invalid-input'))
+})
